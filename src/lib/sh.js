7a3f32b76b6b5f8a772c0ef4696c35e1aa6c977d
Grant Snodgrass <meeber@gametypething.com>
2016-06-07 19:04:21 -0400

    Update eslint-config-tt and fix styles

--- src/lib/sh.js
+++ src/lib/sh.js
@@ -5,7 +5,7 @@ sh.config.fatal = true;
 sh.execAsync = function execAsync (cmd) {
   return new Promise((resolve, reject) => {
     sh.exec(cmd, (code, stdout, stderr) => {
-      let details = {cmd, code, stdout, stderr};
+      const details = {cmd, code, stdout, stderr};
 
       if (code) reject(Object.assign(Error(`${stderr || stdout}`), details));
       else resolve(details);
