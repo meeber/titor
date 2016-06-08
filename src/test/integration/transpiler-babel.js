dd8d72d76174d84d1b8ac40e5752113f4c6ab561
Grant Snodgrass <meeber@gametypething.com>
2016-06-07 21:43:16 -0400

    Refactor TranspilerBabel integration tests

--- src/test/integration/transpiler-babel.js
+++ src/test/integration/transpiler-babel.js
@@ -4,9 +4,6 @@ import TranspilerBabel from "../../lib/transpiler-babel";
 import {cat, cp, test} from "../../lib/sh";
 import {rootDir, standup, teardown} from "../util/fixture";
 
-const curBldDir = join(rootDir, "current-build");
-const legBldDir = join(rootDir, "legacy-build");
-
 describe("TranspilerBabel", () => {
   describe("run in a directory containing a basic src directory", () => {
     before(async () => {
@@ -26,27 +23,38 @@ describe("TranspilerBabel", () => {
       await TranspilerBabel().run();
     });
 
-    it("minimally and recursively transpiles src/ into current-build/", () => {
-      expect(cat(join(curBldDir, "lib/pizza-dog.js")).stdout)
-        .to.match(/exports.default = PizzaDog[\s\S]+let fleas =/);
-      expect(cat(join(curBldDir, "test/unit/pizza-dog.js")).stdout)
-        .to.match(/_pizzaDog = require[\s\S]+let fleas =/);
+    describe("current build", () => {
+      const buildDir = join(rootDir, "current-build");
+
+      it("minimally transpile src/ into current-build/", () => {
+        expect(cat(join(buildDir, "lib/pizza-dog.js")).stdout)
+          .to.match(/exports.default = PizzaDog[\s\S]+let fleas =/);
+      });
+
+      it("recursively transpiles src/ into current-build/", () => {
+        expect(test("-e", join(buildDir, "test/unit/pizza-dog.js"))).to.be.true;
+      });
+
+      it("creates source map files for current-build", () => {
+        expect(test("-e", join(buildDir, "lib/pizza-dog.js.map"))).to.be.true;
+      });
     });
 
-    it("maximally and recursively transpiles src/ into legacy-build/", () => {
-      expect(cat(join(legBldDir, "lib/pizza-dog.js")).stdout)
-        .to.match(/exports.default = PizzaDog[\s\S]+var fleas =/);
-      expect(cat(join(legBldDir, "test/unit/pizza-dog.js")).stdout)
-        .to.match(/_pizzaDog = require[\s\S]+var fleas =/);
-    });
+    describe("legacy build", () => {
+      const buildDir = join(rootDir, "legacy-build");
 
-    it("creates source map files for all transpiled scripts", () => {
-      expect(test("-e", join(curBldDir, "lib/pizza-dog.js.map"))).to.be.true;
-      expect(test("-e", join(curBldDir, "test/unit/pizza-dog.js.map")))
-        .to.be.true;
-      expect(test("-e", join(legBldDir, "lib/pizza-dog.js.map"))).to.be.true;
-      expect(test("-e", join(legBldDir, "test/unit/pizza-dog.js.map")))
-        .to.be.true;
+      it("maximally transpiles src/ into legacy-build/", () => {
+        expect(cat(join(buildDir, "lib/pizza-dog.js")).stdout)
+          .to.match(/exports.default = PizzaDog[\s\S]+var fleas =/);
+      });
+
+      it("recursively transpiles src/ into legacy-build/", () => {
+        expect(test("-e", join(buildDir, "test/unit/pizza-dog.js"))).to.be.true;
+      });
+
+      it("creates source map files for legacy-build", () => {
+        expect(test("-e", join(buildDir, "lib/pizza-dog.js.map"))).to.be.true;
+      });
     });
 
     after(teardown);
