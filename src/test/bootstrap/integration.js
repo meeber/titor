9f9a4bb73b78589c1f6e77fa01c48fb89c308690
Grant Snodgrass <meeber@gametypething.com>
2016-06-20 18:52:09 -0400

    Update eslint-related dependencies

--- src/test/bootstrap/integration.js
+++ src/test/bootstrap/integration.js
@@ -3,6 +3,5 @@
 require("./common");
 
 describe("integration", () => {
-  // eslint-disable-next-line global-require
   require("require-dir")("../integration", {recurse: true});
 });
--- src/test/bootstrap/unit.js
+++ src/test/bootstrap/unit.js
@@ -3,6 +3,5 @@
 require("./common");
 
 describe("unit", () => {
-  // eslint-disable-next-line global-require
   require("require-dir")("../unit", {recurse: true});
 });
